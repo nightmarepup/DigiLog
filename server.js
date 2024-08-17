a4492bede6444fe1903da7e0670b04d7e8d7aa62
Raz <40299471+nightmarepup@users.noreply.github.com>
2024-08-17 19:50:31 +1000

    added test homepage

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const ejsLint = require('ejs-lint');
 var session = require('express-session');
 
 
